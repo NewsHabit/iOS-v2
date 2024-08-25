a4069dcab855cec8eeaff666e6844f56d0102d15
noeyiz <lamoureclatant@gmail.com>
2024-08-25 21:54:37 +0900

    [Feat] 프로젝트 시작 (#2)
    - 프로젝트 생성 및 스토리보드 제거
    - tuist 초기 설정

---
+++ NewsHabit/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsHabit
+//
+//  Created by 지연 on 8/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
