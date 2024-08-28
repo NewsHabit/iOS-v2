5acee45d47ce39b7e53ba9ed0cc17d9a2793507a
noeyiz <lamoureclatant@gmail.com>
2024-08-29 01:51:08 +0900

    design: Color Assets 수정

--- Projects/Shared/DesignSystem/Sources/Colors.swift
+++ Projects/Shared/DesignSystem/Sources/Colors.swift
@@ -10,8 +10,14 @@ import UIKit
 public struct Colors {
     public static let accent = SharedDesignSystemAsset.accent.color
     public static let background = SharedDesignSystemAsset.background.color
+    public static let disabled = SharedDesignSystemAsset.disabled.color
     public static let gray01 = SharedDesignSystemAsset.gray01.color
     public static let gray02 = SharedDesignSystemAsset.gray02.color
     public static let gray03 = SharedDesignSystemAsset.gray03.color
+    public static let gray04 = SharedDesignSystemAsset.gray04.color
+    public static let gray05 = SharedDesignSystemAsset.gray05.color
+    public static let gray06 = SharedDesignSystemAsset.gray06.color
+    public static let gray07 = SharedDesignSystemAsset.gray07.color
+    public static let gray08 = SharedDesignSystemAsset.gray08.color
     public static let primary = SharedDesignSystemAsset.primary.color
 }
