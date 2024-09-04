73c4f808d640ea7b2addff15874dfc389e276a9b
noeyiz <lamoureclatant@gmail.com>
2024-09-05 01:53:25 +0900

    feat: ViewModelProtocol 추가

---
+++ Projects/Shared/Util/Sources/ViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModel.swift
+//  SharedUtil
+//
+//  Created by 지연 on 9/4/24.
+//
+
+import Combine
+import Foundation
+
+public protocol ViewModel {
+    associatedtype Action
+    
+    associatedtype State
+    
+    var cancellables: Set<AnyCancellable> { get set }
+}
